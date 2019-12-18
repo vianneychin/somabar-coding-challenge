603329159cc89fcec33bfe1f5f8f25e6978bad1b
Vianney <vianneyjchin@gmail.com>
2019-12-17 16:58:08 -0800

    fetch api with query

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { SafeAreaView, View, Text, TextInput, StyleSheet } from 'react-native'
 
-const unsplashApi = 'https://api.unsplash.com'
+const accessKey =
+  'ad6b790cd1db3888100bdfcf14d84c73f0e7300f81aec1d891031922857a2e2b'
+const secretKey =
+  '9af57a2345249a02ae5a2a072644aa9ccb05f5f4d755a959d011c7f54f5f4a96'
+
+const unsplashApi = `https://api.unsplash.com/search/photos?client_id=${accessKey}&query=coffee`
 
 const App = () => {
+  useEffect(() => {
+    const fetchApi = async () => {
+      try {
+        const api = await fetch(unsplashApi)
+        const response = api.json()
+        console.log(response)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    // fetchApi()
+  }, [])
+
   return (
     <SafeAreaView style={styles.mainContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.input} />
+        <TextInput
+          placeholder="i.e., cars, coffee, cocktails"
+          style={styles.input}
+        />
       </View>
       <View style={styles.imagesContainer}>
         <Text>Test</Text>
@@ -41,6 +62,8 @@ const styles = StyleSheet.create({
     fontSize: 20,
     borderWidth: 1,
     borderColor: 'rgb(200, 200, 200)',
+    paddingLeft: 20,
+    paddingRight: 20,
   },
 })
 
